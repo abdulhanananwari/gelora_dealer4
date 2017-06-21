9a9a12421f51f6571539064cbb00f38ce4e0a92c
abdulhanan <abdulhananx42@gmail.com>
2017-06-21 10:52:49 +0700

    update actioner

--- Gelora/PolReg/App/LeasingBpkbSubmissionBatch/Managers/Actioners/OnClose.php
+++ Gelora/PolReg/App/LeasingBpkbSubmissionBatch/Managers/Actioners/OnClose.php
@@ -3,6 +3,7 @@
 namespace Gelora\PolReg\App\LeasingBpkbSubmissionBatch\Managers\Actioners;
 
 use Gelora\PolReg\App\LeasingBpkbSubmissionBatch\LeasingBpkbSubmissionBatchModel;
+use Solumax\PhpHelper\App\Mongo\SubDocument;
 
 class OnClose {
 
@@ -13,12 +14,37 @@ class OnClose {
     }
 
     public function action() {
+        
+        $salesOrders = $this->registrationBatch->getSalesOrders();
 
-
-        $this->registrationBatch->closed_at = \Carbon\Carbon::now();
-        $this->registrationBatch->assignEntityData('closer');
-        $this->registrationBatch->save();
+        foreach ($salesOrders as $salesOrder) {
+           
+            $this->assignLeasingBpkbHandover($salesOrder);
+        }
+        \DB::transaction(function() use ($salesOrders) {
+            foreach ($salesOrders as $salesOrder) {
+                $salesOrder->save();
+            }
+            
+            $this->registrationBatch->closed_at = \Carbon\Carbon::now();
+            $this->registrationBatch->assignEntityData('closer');
+            $this->registrationBatch->save();
+        });
+       
         return $this->registrationBatch;
+    }
+    protected function assignLeasingBpkbHandover($salesOrder) {
+        
+        $customerHandover = $salesOrder->polReg;
+       
+        $customerHandover['customer_handovers']['BPKB'] = [
+            'username' => \ParsedJwt::getByKey('name'),
+            'receiver_name' => $this->registrationBatch->receiver_name,
+            'leasing_bpkb_submission_id' => $this->registrationBatch->id,
+            'timestamp' => \Carbon\Carbon::now()->timestamp
+        ];
+
+        $salesOrder->polReg = $customerHandover;
     }
     
 }
