772c456438f8f9df165eded4749f12c090cfcfd3
Reza Riantono <rezariantono@gmail.com>
2017-06-22 21:24:35 +0700

    Changed redirect to auto choose personnel / admin

--- Gelora/Sales/Http/Routes/RedirectApp.php
+++ Gelora/Sales/Http/Routes/RedirectApp.php
@@ -1,9 +1,35 @@
 <?php
 
 Route::group(['prefix' => 'redirect-app'], function() {
-    Route::get('sales-order', function() {
-        $link = "/gelora/sales/admin/index.html#/salesOrder/show/" .
-                request("id", "");
-        return response('', 302, ['Location' => $link]);
+
+    Route::get('sales-order/', function() {
+
+        $viewData = [
+            'link' => request()->url() . '/redirected/?id=' . request()->get('id') . '&jwt=',
+            'id' => request()->get('id', '')
+        ];
+
+        return view('gelora.sales::redirect')
+                        ->with('viewData', $viewData);
+    });
+
+    $middlewares = ['wala.jwt.autoParse.parser', 'wala.jwt.autoParse.validation', 'auth.db.overwrite', 'salesPersonnelAccess'];
+
+    Route::middleware($middlewares)->get('sales-order/redirected', function() {
+
+        $tumr = new \Solumax\AuthClient\Data\TenantUserModuleRole();
+        if ($tumr->check('ADMIN_ACCESS')) {
+            $link = "/gelora/sales/admin/index.html#/salesOrder/show/" .
+                    request("id", "");
+            return response('', 302, ['Location' => $link]);
+        }
+
+        if (request()->has('sales_personnel_access')) {
+            $link = "/gelora/sales/personnel/index.html#/salesOrder/show/" .
+                    request("id", "");
+            return response('', 302, ['Location' => $link]);
+        }
+
+        return 'Anda tidak dapat mengakses halaman ini';
     });
 });
--- Gelora/Sales/Http/routes.php
+++ Gelora/Sales/Http/routes.php
@@ -1,25 +1,18 @@
 <?php
 
 Route::group(['prefix' => 'sales', 'namespace' => 'Gelora\Sales\Http\Controllers'], function() {
-    
+
     include('Routes/Api.php');
     include('Routes/ApiSalesPersonnel.php');
     include('Routes/Report.php');
     include('Routes/Views.php');
-
-});
-
-Route::group(['prefix' => 'sales-admin'], function() {
-    include('Routes/RedirectApp.php'); 
+    include('Routes/RedirectApp.php');
 });
 
 Route::group(['prefix' => 'sales'], function() {
-    
     Route::get('/', function() {
         return redirect("/gelora/sales/personnel/index.html");
     });
-    
-    include('Routes/RedirectApp.php'); 
 });
 
 include('Routes/Views.php');
