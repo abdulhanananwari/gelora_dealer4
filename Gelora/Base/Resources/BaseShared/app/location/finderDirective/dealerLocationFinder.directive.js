fe0f05ef45ec3f996c8f269a222e02805a2d4682
Reza Riantono <rezariantono@gmail.com>
2017-07-01 16:17:17 +0700

    Fixed unit move and updated location finder

--- Gelora/Base/Resources/BaseShared/app/location/finderDirective/dealerLocationFinder.directive.js
+++ Gelora/Base/Resources/BaseShared/app/location/finderDirective/dealerLocationFinder.directive.js
@@ -1,38 +1,35 @@
 geloraBaseShared
-	.directive('dealerLocationFinder', function(
-		$sce, $http, $timeout,
-		LinkFactory,
-		LocationModel) {
+    .directive('dealerLocationFinder', function(
+        $sce, $http, $timeout,
+        LinkFactory,
+        LocationModel) {
 
-		return {
-			templateUrl: $sce.trustAsResourceUrl(LinkFactory.dealer.views.base.locationFinder),
-			restrict: 'E',
-			scope: {
-				location: "=",
-				onLocationSelected: "&"
-			},
-			link: function(scope, elem, attrs) {
+        return {
+            templateUrl: $sce.trustAsResourceUrl(LinkFactory.dealer.views.base.locationFinder),
+            restrict: 'E',
+            scope: {
+                location: "=",
+                onLocationSelected: "&"
+            },
+            link: function(scope, elem, attrs) {
 
-				scope.modalId = Math.random().toString(36).substring(2, 7)
+                scope.modalId = Math.random().toString(36).substring(2, 7)
 
-			
-				LocationModel.index({active: true})
-				.then(function(res) {
-					scope.locations = res.data.data
-				})
+                LocationModel.index({ active: true })
+                    .then(function(res) {
+                        scope.locations = res.data.data
+                    })
 
-				scope.select = function(location) {
+                scope.select = function(location) {
 
-					scope.location = location;
-					$timeout(function() {
-						scope.onLocationSelected();
-					}, 250);
+                    scope.location = location;
+                    $timeout(function() {
+                        scope.onLocationSelected();
+                    }, 250);
 
-				}
+                }
 
+            },
+        }
 
-
-			},
-		}
-
-	})
+    })
