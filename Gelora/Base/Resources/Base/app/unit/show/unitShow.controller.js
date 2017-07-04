ed2f713730f48f53dbdcb515a59da7fb32f810f7
Reza Riantono <rezariantono@gmail.com>
2017-07-04 09:37:48 +0700

    Added PDI and receive on  unit

--- Gelora/Base/Resources/Base/app/unit/show/unitShow.controller.js
+++ Gelora/Base/Resources/Base/app/unit/show/unitShow.controller.js
@@ -1,14 +1,50 @@
 geloraBase
-	.controller('UnitShowController', function(
-		$state,
-		UnitModel) {
+    .controller('UnitShowController', function(
+        $state,
+        UnitModel) {
 
-		var vm = this
+        var vm = this
 
-		vm.id = $state.params.id
+        vm.id = $state.params.id
 
-		UnitModel.get($state.params.id)
-		.then(function(res) {
-			vm.unit = res.data.data
-		})
-	})
+        UnitModel.get($state.params.id)
+            .then(function(res) {
+                vm.unit = res.data.data
+            })
+
+        vm.action = {
+            receive: function(unit, location) {
+                UnitModel.action.receive(unit.id, { location_id: location.id })
+                    .then(function(res) {
+                        vm.unit = res.data.data
+                    })
+            },
+            pdi: function(unit) {
+
+                var dialog = $('<p>PDI berhasil dan unit dalam keadaan baik?</p>').dialog({
+                    buttons: {
+                        "Yes": function() {
+
+                            UnitModel.action.pdi(unit.id, { pdi_success: true })
+                                .then(function(res) {
+                                    vm.unit = res.data.data
+                                    dialog.dialog('close')
+                                })
+                        },
+                        "Tidak": function() {
+                            UnitModel.action.pdi(unit.id, { pdi_success: false })
+                                .then(function(res) {
+                                    vm.unit = res.data.data
+                                    dialog.dialog('close')
+                                })
+                        },
+                        "Batal": function() {
+
+                            dialog.dialog('close')
+                        }
+                    }
+                })
+
+            }
+        }
+    })
