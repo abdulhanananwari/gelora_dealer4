9ed672920609701ac4de1008f964b520b8767cbb
Reza Riantono <rezariantono@gmail.com>
2017-07-12 15:34:35 +0700

    Updated JP updater

--- Gelora/Sales/App/SalesOrder/Managers/Assigners/Specific/LeasingOrder.php
+++ Gelora/Sales/App/SalesOrder/Managers/Assigners/Specific/LeasingOrder.php
@@ -13,9 +13,17 @@ class LeasingOrder {
     }
     
     public function assign($leasingOrder) {
-
+        
+        // Copy dulu data JP
+        $joinPromos = $this->salesOrder->getAttribute('leasingOrder.joinPromos');
+        
         $this->salesOrder->leasingOrder = $leasingOrder;
-
+        
+        // Kalau user ga punya akses liat JP, balikin data JP dengan yang lama
+        if (!\SolAuthClient::hasAccess('VIEW_LEASING_ORDER_JOIN_PROMOS')) {
+            $this->salesOrder->setAttribute('leasingOrder.joinPromos', $joinPromos);
+        }
+        
         return $this->salesOrder;
     }
 }
