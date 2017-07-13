a6fa8a39e9e3de3bab00e1d0ae4e67f945eff479
abdulhanan <abdulhananx42@gmail.com>
2017-07-13 17:09:41 +0700

    update print

--- Gelora/Sales/Resources/Views/financial/generateCustomerInvoice.blade.php
+++ Gelora/Sales/Resources/Views/financial/generateCustomerInvoice.blade.php
@@ -1,95 +1,62 @@
-<!DOCTYPE html>
-<html>
+@extends('layout.printing')
 
-<head>
-    <title>Tagihan Customer #{{ $viewData['salesOrder']->id }}</title>
-    <link rel="stylesheet" type="text/css" href="/standard/bootstrap-3.3.6-dist/css/bootstrap.min.css">
-    <style type="text/css">
-    @media print {
-        html, body {
-            width: 100%;
-        }
-    }
-    @page {
-        size: A4 portrait;
-    }
+@section('title', 'Bukti Tagihan Ke Konsumen')
 
-    .container {
-        width: 100%;
-        font-family: 'Courier New' !important;
-        font-size: 2em !important;
-        letter-spacing: 5px !important;
-    }
-    .table {
-        border-style: none !important;
-        border: 0 !important;
-    }
-    
-    .layout-table > tbody > tr > td {
-        border-top: 0px !important;
-        vertical-align: top;
-    }
-    
-    p {
-        margin: 0;
-    }
-    </style>
-</head>
-
-<body>
-    <div class="container">
+@section('content')
+ <div class="container">
         <div class="row">
             <div class="col-sm-12">
-                <table class="table layout-table" style="width :  100%; margin-bottom: 5px;">
-                    <tr>
-                        <td style="width :  8em; padding-right :  10px;">
-                            <img style="width: auto; max-height: 5em !important;" class="img-responsive" src="{{ $viewData['tenantInfo']->LOGO }}">
-                        </td>
-                        <td>
-                            <p><strong>{{ $viewData['tenantInfo']->TENANT }}</strong></p>
-                            <p>{{ $viewData['tenantInfo']->ADDRESS }}</p>
-                            <p>{{ $viewData['tenantInfo']->PHONE_NUMBER }}</p>
-                        </td>
-                        <td>
-                            <p class="text-right">Tagihan SPK {{ $viewData['salesOrder']->id }}</p>
-                            <img style="height: auto; max-width: 20em !important;" class="img-responsive pull-right" src="{{ $viewData['salesOrder']->retrieve()->barcode() }}">
-                        </td>
-                    </tr>
-                </table>
+                <div class="row">
+                    <div class="col-xs-1">
+                        <img style="width: 100% !important; height: auto;" src="{{ $viewData['tenantInfo']->LOGO }}">
+                    </div>
+                    <div class="col-xs-7">
+                        <p><strong>{{ $viewData['tenantInfo']->TENANT }}</strong></p>
+                        <p>{{ $viewData['tenantInfo']->ADDRESS }}</p>
+                        <p>{{ $viewData['tenantInfo']->PHONE_NUMBER }}</p>
+                    </div>
+                    <div class="col-xs-4">
+                        <p class="text-right"><strong>Tagihan SPK {{ $viewData['salesOrder']->id }}</strong></p>
+                        <img style="height: auto; max-width: 20em !important;" class="img-responsive pull-right" src="{{ $viewData['salesOrder']->retrieve()->barcode() }}">
+                    </div>
+                </div>
                 <h2 class="text-center">INVOICE / TAGIHAN</h2>
+                <div class="row">
+                    <div class="col-xs-8">
+                        <table width="100%">
+                            <tr>
+                                <td style="width: 50%;">Nama customer</td>
+                                <td style="width: 50%; padding-left: 5px;">{{ $viewData['salesOrder']->customer['name'] }}</td>
+                            </tr>
+                            <tr>
+                                <td style="font-size: 1.2em;">Total Terhutang</td>
+                                <td style="padding-left: 5px;"><strong>Rp {{ number_format($viewData['balance']['payment_unreceived']) }}</strong></td>
+                            </tr>
+                            <tr>
+                                <td style="font-size: 1.2em;">Total Tagihan Ini</td>
+                                <td style="padding-left: 5px;"><strong>Rp {{ number_format($viewData['invoiceAmount'])}}</strong></td>
+                            </tr>
+                            
+                            <tr>
+                                <td>Untuk</td>
+                                <td style="padding-left: 5px;">Pelunasan SPK {{ $viewData['salesOrder']->id }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="col-xs-4">
+                        <table width="100%">
+                            <tr>
+                                <td><p>Tanggal SPK: {{$viewData['salesOrder']->created_at->toDateTimeString() }}</p>
+                                    @if( $viewData['salesOrder']->validated_at )
+                                    <p>Tanggal Validasi: {{$viewData['salesOrder']->validated_at->toDateTimeString() }}</p>
+                                    @endif
+                                    <p>Tanggal Cetak: {{ \Carbon\Carbon::now()->toDateTimeString() }}</p>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
                 <hr>
-                <table style="width: 100%">
-                    <tr>
-                        <td style="width: 70%;">
-                            <table>
-                                <tr>
-                                    <td style="width: 50%;">Nama customer</td>
-                                    <td style="width: 50%; padding-left: 5px;">{{ $viewData['salesOrder']->customer['name'] }}</td>
-                                </tr>
-                                <tr>
-                                    <td style="font-size: 1.2em;">Total Terhutang</td>
-                                    <td style="padding-left: 5px;"><strong>Rp {{ number_format($viewData['balance']['payment_unreceived']) }}</strong></td>
-                                </tr>
-                                <tr>
-                                    <td style="font-size: 1.2em;">Total Tagihan Ini</td>
-                                    <td style="padding-left: 5px;"><strong>Rp {{ number_format($viewData['invoiceAmount'])}}</strong></td>
-                                </tr>
-                                
-                                <tr>
-                                    <td>Untuk</td>
-                                    <td style="padding-left: 5px;">Pelunasan SPK {{ $viewData['salesOrder']->id }}</td>
-                                </tr>
-                            </table>
-                        </td>
-                        <td style="width: 30%;" valign="top">
-                            <p>Tanggal SPK: {{$viewData['salesOrder']->created_at->toDateTimeString() }}</p>
-                            @if( $viewData['salesOrder']->validated_at )
-                                <p>Tanggal Validasi: {{$viewData['salesOrder']->validated_at->toDateTimeString() }}</p>
-                            @endif
-                            <p>Tanggal Cetak: {{ \Carbon\Carbon::now()->toDateTimeString() }}</p>
-                        </td>
-                    </tr>
-                </table>
                 <br>
                 <p><strong>Petunjuk Pembayaran:</strong></p>
                 <ul>         
@@ -126,9 +93,4 @@
             </div>
         </div>
     </div>
-    <script type="text/javascript">
-    window.print();
-    </script>
-</body>
-
-</html>
+@endsection
