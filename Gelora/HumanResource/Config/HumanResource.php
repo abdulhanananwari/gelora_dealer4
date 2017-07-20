1fa11e980dae073de6295c76cf5714dec5734eae
Reza Riantono <rezariantono@gmail.com>
2017-07-20 14:09:54 +0700

    Penambahan posisi

--- Gelora/HumanResource/Config/HumanResource.php
+++ Gelora/HumanResource/Config/HumanResource.php
@@ -6,6 +6,9 @@ return [
         'JuniorSalesman' => 'Sales Junior',
         'SeniorCoordinator' => 'Koordinator Senior',
         'JuniorCoordinator' => 'Koordinator Junior',
+        'Office' => 'Office',
+        'Channel' => 'Channel',
+        'ChannelLeasing' => 'Channel Leasing',
     ],
     
     'defaultTumrs' => ['VIEW_SALES_PLAFOND', 'VIEW_PLAFOND']
