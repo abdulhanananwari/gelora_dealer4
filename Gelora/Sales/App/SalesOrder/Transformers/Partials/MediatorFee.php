07768bf3296c5dacd36beb50104822763ffcd12a
abdulhanan <abdulhananx42@gmail.com>
2017-07-28 16:23:15 +0700

    update filter statuses

---
+++ Gelora/Sales/App/SalesOrder/Transformers/Partials/MediatorFee.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Gelora\Sales\App\SalesOrder\Transformers\Partials;
+
+use \Solumax\PhpHelper\App\Mongo\SubDocument;
+use MongoDB\BSON\UTCDateTime;
+
+class MediatorFee {
+
+    public static function transform(SalesOrderModel $salesOrder) {
+
+        $data = [
+            
+        ];
+
+        return $data;
+    }
+
+}
