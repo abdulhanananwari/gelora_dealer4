77bef3bcef7a6e848ea8716993b70e3e0c338252
Reza <rezariantono@gmail.com>
2017-07-29 14:47:27 +0700

    Added personnel pagination

--- Gelora/HumanResource/Http/Routes/Trigger.php
+++ Gelora/HumanResource/Http/Routes/Trigger.php
@@ -3,31 +3,52 @@
 $middleware = ['auth.db.manualOverwrite:tenantId'];
 
 Route::group(['prefix' => '{tenantId}/trigger', 'middleware' => $middleware], function() {
-    
+
     Route::get('update-personnel-name', function(Illuminate\Http\Request $request) {
-       
+
         $personnels = Gelora\HumanResource\App\Personnel\PersonnelModel::get();
-        
+
         foreach ($personnels as $personnel) {
-            
+
             $personnel->setAttribute('name', $personnel->getAttribute('entity.name'));
             $personnel->save();
         }
-        
+
         if ($request->get('so_too') == 'true') {
-            
+
             $salesOrders = \Gelora\Sales\App\SalesOrder\SalesOrderModel::get();
             foreach ($salesOrders as $salesOrder) {
-                
+
                 $salesName = $salesOrder->getAttribute('salesPersonnel.entity.name');
                 if ($salesName) {
                     $salesOrder->setAttribute('salesPersonnel.name', $salesName);
                     $salesOrder->save();
                 }
-                
             }
         }
-        
-        
+    });
+    
+    Route::get('update-personnel-name', function(Illuminate\Http\Request $request) {
+
+        $personnels = Gelora\HumanResource\App\Personnel\PersonnelModel::get();
+
+        foreach ($personnels as $personnel) {
+
+            $personnel->setAttribute('name', $personnel->getAttribute('entity.name'));
+            $personnel->save();
+        }
+
+        if ($request->get('so_too') == 'true') {
+
+            $salesOrders = \Gelora\Sales\App\SalesOrder\SalesOrderModel::get();
+            foreach ($salesOrders as $salesOrder) {
+
+                $salesName = $salesOrder->getAttribute('salesPersonnel.entity.name');
+                if ($salesName) {
+                    $salesOrder->setAttribute('salesPersonnel.name', $salesName);
+                    $salesOrder->save();
+                }
+            }
+        }
     });
 });
