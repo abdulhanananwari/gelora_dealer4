103abe03cd1901e3dbb706701028c10966804fbb
Reza Riantono Sukarno <rezariantono@gmail.com>
2017-08-05 13:56:43 +0700

    Fixed delivery handover file upload and added fields to report

--- Gelora/Sales/Resources/SalesShared/app/salesOrder/report/salesOrderReport.controller.js
+++ Gelora/Sales/Resources/SalesShared/app/salesOrder/report/salesOrderReport.controller.js
@@ -17,9 +17,22 @@ geloraSalesShared
             window.open(LinkFactory.dealer.sales.salesOrder.report + '?' + paramString)
         }
 
-        vm.consoleLog = function(key, value) {
-            console.log(key)
-            console.log(value)
+        vm.massUpdate = function(action) {
+
+            if (typeof vm.filter.fields == 'undefined') { vm.filter.fields = {} }
+
+            if (action) {
+
+                _.each(vm.filteredFields, function(field) {
+                    vm.filter.fields[field] = 'true'
+                })
+
+            } else {
+
+                _.each(vm.filteredFields, function(field) {
+                    vm.filter.fields[field] = 'false'
+                })
+            }
         }
 
         vm.fields = [
@@ -34,9 +47,23 @@ geloraSalesShared
             'ALAMAT STNK',
             'NOMOR TELEPON STNK',
             'NOMOR KTP STNK',
+            'KOTA STNK',
+            'KECAMATAN STNK',
+            'KELURAHAN STNK',
+            'KODE POS STNK',
             'NAMA KIRIM',
             'ALAMAT KIRIM',
             'NOMOR TELEPON KIRIM',
+            'NAMA PENERIMA',
+            'NOMOR TELEPON PENERIMA',
+            'CATATAN PENERIMAAN',
+            'FOTO KTP PENERIMA',
+            'FOTO SERAH TERIMA',
+            'WAKTU SERAH TERIMA',
+            'LATITUDE SERAH TERIMA',
+            'LONGITUDE SERAH TERIMA',
+            'WAKTU BASTK',
+            'ADMIN BASTK',
             'EXTRA',
             'NOMOR PO',
             'NOMOR APLIKASI',
@@ -60,7 +87,7 @@ geloraSalesShared
             'MEDIATOR',
             'ID SALES',
             'NAMA SALES',
-            'TANGGAL TUTUP',
+            'NAMA TEAM SALES',
             'NAMA PILIHAN TYPE MOTOR',
             'KODE PILIHAN TYPE MOTOR',
             'NAMA PILIHAN WARNA MOTOR',
@@ -69,16 +96,10 @@ geloraSalesShared
             'NOMOR RANGKA',
             'NOMOR MESIN',
             'TANGGAL DO',
-            'PENUTUP',
-            'NAMA TEAM SALES',
-            'KOTA STNK',
-            'KECAMATAN STNK',
-            'KELURAHAN STNK',
             'KOTA ( UNTUK FAKTUR )',
             'KECAMATAN ( UNTUK FAKTUR )',
             'KELURAHAN ( UNTUK FAKTUR ) ',
             'ALAMAT (UNTUK FAKTUR )',
-            'KODE POS STNK',
             'KODE POS ( UNTUK FAKTUR )'
         ]
 
